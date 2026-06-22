47c12936ccf5d5d74f427f4561a73297319a2f59
Arav Jain <240245776+Unstoppable-AJ-LimitsNotFound@users.noreply.github.com>
2026-06-22 05:54:42 +0000

    Create functions for basic math operations

---
+++ script.js
@@ -0,0 +1,10 @@
+//Basic Functions
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+// console.log(add(10, 2));
+// console.log(subtract(10, 2));
+// console.log(multiply(10, 2));
+// console.log(divide(10, 2));
